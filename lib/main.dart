0b1610e8ad5aa2de5fcdd1dde0cb9c8e338e8184
mr cho <kyuseong2001@gmail.com>
2021-12-31 09:42:47 +0700

    '20211231'

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -11,56 +10,67 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home:Scaffold(
-        appBar: AppBar(title: Text('Rent Car'),
-        leading : Icon(Icons.menu),
-        ),
-      body:Container(
-        height:150,
-        child: Row(
-          children: [
-            Image.asset('assets/camera.jpg', width: 140),
-            Expanded(
-              child:Container(
-                margin:EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:[
-                    Text('카메라 팝니다',style:TextStyle(fontWeight: FontWeight.w700)),
-                    Text('금호동 3가'),
-                    Text('7000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: const [
-                        Icon(Icons.favorite),
-                        Text('4')
+
+
+        appBar: AppBar(title: Text('Danggun Market'),
+          leading: Icon(Icons.menu)),
+
+        body:Container(
+          height:130,
+          child:Row(
+            children: [
+              Image.asset('assets/camera.jpg'),
+              Expanded(
+                  child:Container(
+                    margin:EdgeInsets.all(10),
+                    child:Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                        Text('카메라 팝니다.',style:TextStyle(fontWeight:FontWeight.w700, fontSize: 20)),
+                        Text('성수동 2가',style: TextStyle(fontSize: 18),),
+                        Text('7000원',style:TextStyle(fontWeight:FontWeight.w900,fontSize: 17)),
+                        Container(
+
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.favorite),
+                            Text('2'),
+                           ],
+                          ),
+                        ),
+
+
                       ],
                     )
-                  ],
-                ),
-              )
-            )
-          ],
+                  ))
+            ],
+          ),
         ),
-      ),
+
 
         bottomNavigationBar: BottomAppBar(
           child:Container(
-            margin:EdgeInsets.all(10),
-            padding:EdgeInsets.all(10),
+            margin:EdgeInsets.all(20),
+            padding:EdgeInsets.fromLTRB(10, 10,10,10),
+
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
               children: [
                 Icon(Icons.phone),
                 Icon(Icons.message),
                 Icon(Icons.contact_page)
-                        ],
-                ),
+              ],
+            ),
           ),
         ),
 
-        ),
-      );
 
+
+
+      ),
+    );
   }
 }
+
+
