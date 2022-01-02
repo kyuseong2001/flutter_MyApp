165f1a196c3e6b8371b09b22760303d905f6a529
mr cho <kyuseong2001@gmail.com>
2022-01-02 20:07:31 +0700

    '20220102'

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(const MyApp());
 }
-
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var a =1;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child:Text(a.toString()),
-          onPressed:(){
-            setState(() {
-              a++;
-            });
-          } ,
-
-        ),
-        appBar: AppBar(title:Text('eunseo market'),
-         leading: Icon(Icons.menu),
+      home: Scaffold(
+          appBar:AppBar(title: Text('eunseo'),
+            leading: Icon(Icons.menu),
           ),
-
-
-        body: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (context,i){
-            return ListTile(
-              leading: Icon(Icons.account_circle),
-              title:Text('홍길동'),
-            );
-          },
-        ),
-
-
-
-
-
-
-
-
-
-
-      bottomNavigationBar: BottomAppBar(),
+          body:ListView.builder(
+              itemCount:300,
+              itemBuilder:(context,i){
+                return ListTile(
+                  leading: Icon(Icons.account_circle),
+                  title:Text('홍길동'),
+                );
+              })
       ),
+
     );
-  }
-}
-
-class BottomAppBar extends StatelessWidget {
-  const BottomAppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-          margin:EdgeInsets.all(20),
-          child:Row(
-            mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-            children: [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.contact_page)
-            ],
-          )
-      ),
-    );
-
   }
 }
