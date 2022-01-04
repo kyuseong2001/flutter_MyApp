599ea89d35cdaf77195a329207099d1a65ef46a4
mr cho <kyuseong2001@gmail.com>
2022-01-04 16:30:16 +0700

    '20220104'

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -10,19 +11,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar:AppBar(title: Text('eunseo'),
-            leading: Icon(Icons.menu),
-          ),
-          body:ListView.builder(
-              itemCount:300,
-              itemBuilder:(context,i){
-                return ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title:Text('홍길동'),
-                );
-              })
-      ),
 
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+          title: Text('렌트카 어플'),
+        ),
+
+
+        body: Row(
+          children: [
+            Image.asset('assets/camera.jpg'),
+            Expanded( // Expanded는 위에 이미지와 그옆에 모든 칸을 채우기를 원할때 사용한다.
+              child: Container(
+                height: 100,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: [
+                    Text('강낭콩 팝니다.',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.red),
+                    ),
+                    Text('몸에좋아요',
+                      style: TextStyle(
+                          fontSize:10,
+                          color: Colors.black),
+                    ),
+                    Row(
+                      children: const [
+                        Icon(Icons.favorite),
+                        Text('2'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+
+        bottomNavigationBar: BottomAppBar(),
+      ),
     );
+
+  }
+}
+
+class BottomAppBar extends StatelessWidget {
+  const BottomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Icon(Icons.phone),
+          Icon(Icons.message),
+          Icon(Icons.contact_page),
+        ],
+      ),
+    );
+
   }
 }
