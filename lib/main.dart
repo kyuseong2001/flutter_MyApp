b878435a1374d38addeebc12d41b45983489010a
mr cho <kyuseong2001@gmail.com>
2022-01-05 22:44:07 +0700

    '20220105'

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+
+
+class MyApp extends StatefulWidget {
+   MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  var name = ['김영숙','피자집','홍길동'];
+  var like = [0,0,0];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-
-        appBar: AppBar(
-          leading: Icon(Icons.menu),
-          title: Text('렌트카 어플'),
+        floatingActionButton: FloatingActionButton(
+          child:Text(a.toString()) ,  //feb
+          onPressed: (){
+            setState(() {
+              //스테이트를 변경하는 방법으로 스테이트 풀위젯으로 변경한 후 set setstate 함수를 써서 변경가능
+              a++;
+            });
+          },
+          // 버튼눌렀을때 코드실행해주는 코드
+          // 잠깐 데이터를 저장할 때 변수나 스테이트를 사용해서 자료저장한다.
+          // 재 랜더링이 안될때는 스테이트 풀 위젯으로 변경해야한다.
         ),
+        appBar: AppBar(),
+        body:ListView.builder(
+        itemCount: 3,
+        itemBuilder: (context, i) {
+          return ListTile(
+            leading: Text(like[i].toString()),
+            title: Text(name[i]),
+            trailing:ElevatedButton( child: Text('좋아요'), onPressed: (){
+              setState(() {
+                like[i]++;
+              });
 
-
-        body: Row(
-          children: [
-            Image.asset('assets/camera.jpg'),
-            Expanded( // Expanded는 위에 이미지와 그옆에 모든 칸을 채우기를 원할때 사용한다.
-              child: Container(
-                height: 100,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-
-                  children: [
-                    Text('강낭콩 팝니다.',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.red),
-                    ),
-                    Text('몸에좋아요',
-                      style: TextStyle(
-                          fontSize:10,
-                          color: Colors.black),
-                    ),
-                    Row(
-                      children: const [
-                        Icon(Icons.favorite),
-                        Text('2'),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-
-        bottomNavigationBar: BottomAppBar(),
+            } ),
+          );
+       },
       ),
-    );
-
-  }
-}
-
-class BottomAppBar extends StatelessWidget {
-  const BottomAppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(Icons.phone),
-          Icon(Icons.message),
-          Icon(Icons.contact_page),
-        ],
-      ),
-    );
-
+    ));
   }
 }
