0425dba605f161a608a83f90ac40313010b10a4c
mr cho <kyuseong2001@gmail.com>
2022-01-08 08:59:11 +0700

    '20220108'

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,51 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-var a = 1;
-var like = [0, 0, 0];
-var name = ['김영숙', '홍길동', '피자집'];
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Shoppy'),
-        leading: Icon(Icons.menu),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.search_rounded),
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (c, i) {
-          return ListTile(
-            leading: Text(like[i].toString()),
-            title: Text(name[i]),
-            trailing: ElevatedButton(
-                child: Text('좋아요'),
-                onPressed: () {
-                  setState(() {
-                    like[i]++;
-                  });
-                }),
+      home: Scaffold(
+        floatingActionButton: Builder(builder: (context) {
+          return FloatingActionButton(
+            child: Text('button'),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    child: Column(
+                      children: [
+                        Container(
+                          child: TextField(
+                            style: TextStyle(fontSize: 15, color: Colors.black38),
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(hintText: '입력해 주세요'),
+                          ),
+                        ),
+                        ElevatedButton(
+                          child: Text('Cancel'),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
           );
-        },
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          margin: EdgeInsets.all(15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.contact_page),
-            ],
-          ),
+        }),
+        appBar: AppBar(
+          title: Text('Shoppy'),
         ),
       ),
-    ));
+    );
   }
 }
