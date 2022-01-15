9592799be1f0553570f6d8aa3572d2d1cf6a2167
mr cho <kyuseong2001@gmail.com>
2022-01-15 17:11:59 +0700

    '20220115'

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp( MaterialApp(
-      home: MyApp()
+      home : MyApp()
   ));
 }
 
 
-
 class MyApp extends StatefulWidget {
-   MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -18,108 +16,82 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  var total = 0;
-  var name = ['a','b','v'];
-  var like = [0];
+  var name = ['조규성','조연우','조은서'];
+  var a = 3;
+  var total =3; // 총 친구숫자
 
 
-addOne(){
-  setState(() {
-    total++;
-  });
-}
+  addName(a){
+    setState(() {
+      name.add(a);
+    });
+  }
 
-addName(a){
-  setState(() {
-    name.add(a);
-  });
-}
-
-// 자식 위젯이 부모의 위젯을 변경하고 싶을때
-  // 1. 변경하고자 하는 함수를 먼저 만든다.
-
-
+  addOne(){
+    setState(() {
+      total++;
+    });
+  }
 
   @override
-  build(context) {
+  Widget build(BuildContext context) {
+    // context 부모위젯의 정보를 담고있는 변수 = 족보
+    // 부모중에 material app 이 들어있는 context를 입력해야 잘 동작하는 함수이다.
     return Scaffold(
-      appBar: AppBar(
-        leading:Icon(Icons.menu),
-        title:Text(total.toString())),
-
-        body: ListView.builder(
-          itemCount:name.length,
-          itemBuilder: (context,i){
-            return ListTile(
-              leading: Text(like[i].toString()),
-              title: Text(name[i]),
-              trailing:
-              ElevatedButton( child: Text('+'),
-                onPressed: (){
-                  setState(() {
-                    like[i]++;
-                    });
-                  } )
-              );
-            }
-          ),
-
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          showDialog(context: context,builder: (context){
-            return DialogUI(addOne:addOne, addName: addName);
-            // 부모정보를 자식에게 보내는 방법
-            // 1. return DialogUI(작명(state):보낼것(변수));
-            // 2. 자식위젯에 등록 this.state
-            // 3. 자식위젯 보여줄곳에 등록 state.toString()
-
-            // 자식위젯이 부모의 위젯을 변경할때
-            // return DialogUI(addOne:addOne);
+          showDialog(context: context, builder: (context){
+            return DialogUI(addOne : addOne, addName : addName);   // 부모정보를 자식state에 전송하는법
+            // 1. 작명:보내려는 변수
           });
         },
       ),
+
+
+      appBar: AppBar(title:Text(total.toString())),
+      body:ListView.builder(
+          itemCount:name.length,
+          itemBuilder:(c,i){
+            return ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text(name[i]),
+            );
+          }),
     );
+
   }
 }
 
 
 class DialogUI extends StatelessWidget {
-   DialogUI({Key? key,this.addOne, this.addName}) : super(key: key);
-   final addOne; //등록 끝
-   final addName;
+  DialogUI({Key? key,this.addOne, this.addName}) : super(key: key);
 
+  final addOne;
+  final addName;
+
+  // var inputData = TextEditingController();
   var inputData = TextEditingController();
-  // 인풋데이터라는 변수에 입력한 내용을 잠깐 저장하고싶을때 사용하는 함수
- // 다음 텍스트 필드에 controler:inputData(변수명)
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(  //모달창의 border.radious
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(25.0))),
-      child: Container(
-          width: 250,
-          height: 150,
-          padding: EdgeInsets.all(20),
-
+    return Dialog(
+        child:SizedBox(
+          height:200,
+          width: 300,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-
             children: [
-              TextField( controller: inputData,),
-              TextButton(child:Text('Confirm'),onPressed: (){
-                addOne();
-                addName(inputData.text);
-
-                Navigator.pop(context);
-              },),
-              TextButton(child:Text('Cancel'),onPressed: (){
-                Navigator.pop(context);
-              },)
+              TextField(controller:inputData,), // 인픗데이터에는 변수로저장이 된다.
+              TextButton(child: Text('완료'),
+                onPressed:(){
+                  addOne();
+                  addName(inputData.text);
+                  Navigator.pop(context);
+                }, ),
+              TextButton(child: Text('취소'),
+                  onPressed:(){Navigator.pop(context); }) // 버튼누르면 없어짐
             ],
-          )
-      ),
+          ),
+        )
     );
   }
 }
