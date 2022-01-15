da646d489249353cd10c214805b1f1fd59a610d8
mr cho <kyuseong2001@gmail.com>
2022-01-15 23:36:34 +0700

    '20220115'

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp( MaterialApp(
-      home : MyApp()
+      home:MyApp()
   ));
 }
-
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -16,82 +15,110 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  var name = ['조규성','조연우','조은서'];
-  var a = 3;
-  var total =3; // 총 친구숫자
 
 
-  addName(a){
-    setState(() {
-      name.add(a);
-    });
-  }
-
-  addOne(){
-    setState(() {
-      total++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // context 부모위젯의 정보를 담고있는 변수 = 족보
-    // 부모중에 material app 이 들어있는 context를 입력해야 잘 동작하는 함수이다.
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          showDialog(context: context, builder: (context){
-            return DialogUI(addOne : addOne, addName : addName);   // 부모정보를 자식state에 전송하는법
-            // 1. 작명:보내려는 변수
-          });
-        },
-      ),
+var total = 0;
+var name = ['조규성','조연우'];
 
 
-      appBar: AppBar(title:Text(total.toString())),
-      body:ListView.builder(
-          itemCount:name.length,
-          itemBuilder:(c,i){
-            return ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text(name[i]),
-            );
-          }),
-    );
 
-  }
+
+addOne(){
+  setState(() {
+    total++;
+  });
+}
+
+addName(a){
+  setState(() {
+    name.add(a);
+  });
 }
 
 
-class DialogUI extends StatelessWidget {
-  DialogUI({Key? key,this.addOne, this.addName}) : super(key: key);
 
-  final addOne;
-  final addName;
-
-  // var inputData = TextEditingController();
-  var inputData = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-        child:SizedBox(
-          height:200,
+    return Scaffold(
+
+        floatingActionButton: FloatingActionButton(
+          child: Text('Button'),
+          onPressed: (){
+            showDialog(context: context, builder:(context){
+              return DialogUI(addOne:addOne, addName: addName);
+
+            });
+          },
+        ),
+
+
+
+
+
+
+
+        appBar:AppBar(
+          leading: Icon(Icons.menu),
+          title: Text(total.toString()),
+          actions: [Icon(Icons.search_rounded)],
+        ),
+
+
+        body:ListView.builder(
+          itemCount: name.length,
+          itemBuilder: (context,i){
+            return ListTile(
+
+              leading: Icon(Icons.account_circle),
+              title:Text(name[i]),
+              trailing:ElevatedButton(child: Text('좋아요'),onPressed: (){
+                addOne();
+
+              },),
+            );
+          },
+
+        ),
+
+    );
+  }}
+
+
+  class DialogUI extends StatefulWidget {
+    DialogUI({Key? key,this.addOne,this.addName}) : super(key: key);
+    final addOne;
+    final addName;
+
+  @override
+  State<DialogUI> createState() => _DialogUIState();
+}
+
+class _DialogUIState extends State<DialogUI> {
+    var inputData = TextEditingController();
+
+    @override
+    Widget build(BuildContext context) {
+      return Dialog(
+        child: SizedBox(
           width: 300,
+          height:200,
           child: Column(
             children: [
-              TextField(controller:inputData,), // 인픗데이터에는 변수로저장이 된다.
-              TextButton(child: Text('완료'),
-                onPressed:(){
-                  addOne();
-                  addName(inputData.text);
-                  Navigator.pop(context);
-                }, ),
-              TextButton(child: Text('취소'),
-                  onPressed:(){Navigator.pop(context); }) // 버튼누르면 없어짐
+              TextField(controller:inputData,),
+              TextButton(onPressed: (){
+                widget.addOne();
+                widget.addName(inputData.text);
+
+              }, child: Text('완료')),
+              TextButton(onPressed: (){
+                Navigator.pop(context);
+              }, child: Text('취소')),
+
             ],
           ),
-        )
-    );
-  }
+        ),
+      );
+    }
 }
+
