f62e0ea629d449515aef744a3d94f77809b10e6f
mr cho <kyuseong2001@gmail.com>
2022-01-16 15:49:43 +0700

    '20220116'

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 
 void main() {
   runApp(MaterialApp(home: MyApp()));
@@ -12,6 +14,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+
+  getPermission() async {
+    var status = await Permission.contacts.status;
+    if (status.isGranted) {
+      print('허락됨');
+    } else if (status.isDenied) {
+      print('거절됨');
+      Permission.contacts.request();  // 팝업창 띄워주세요
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getPermission();
+  }
+  // 이 위젯이 처음 로드가될때 실행해주세요
+
+
   var total = 0;
   var like = 0;
   var name = [];
@@ -28,8 +51,6 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  var inputData = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,36 +60,23 @@ class _MyAppState extends State<MyApp> {
             showDialog(
                 context: context,
                 builder: (context) {
-                  return Dialog(
-                    child: SizedBox(
-                      width: 300,
-                      height: 200,
-                      child: Column(
-                        children: [
-                          TextField(
-                            controller: inputData,
-                          ),
-                          TextButton(
-                              onPressed: () {
-                                addName(inputData.text);
-                                Navigator.pop(context);
-                              },
-                              child: Text('완료')),
-                          TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: Text('취소')),
-                        ],
-                      ),
-                    ),
-                  );
+                  return DialogUI(addName: addName,);
                 });
           }),
       appBar: AppBar(
         leading: Icon(Icons.menu),
         title: Text('shoppy'),
+        actions: [
+          IconButton(onPressed: (){getPermission();}, icon: Icon(Icons.contacts),),],
       ),
+
+
+
+
+
+
+
+
       body: ListView.builder(
         itemCount: name.length,
         itemBuilder: (context, i) {
@@ -76,7 +84,7 @@ class _MyAppState extends State<MyApp> {
             leading: Icon(Icons.account_circle),
             title: Text(name[i]),
             trailing: ElevatedButton(
-              child: Text('좋아요'),
+              child: Text('삭제'),
               onPressed: () {
                 setState(() {
                   like++;
@@ -97,6 +105,47 @@ class _MyAppState extends State<MyApp> {
               Icon(Icons.message),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class DialogUI extends StatefulWidget {
+  const DialogUI({Key? key, this.addName}) : super(key: key);
+
+  final addName;
+
+  @override
+  State<DialogUI> createState() => _DialogUIState();
+}
+
+class _DialogUIState extends State<DialogUI> {
+  var inputData = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 200,
+        child: Column(
+          children: [
+            TextField(
+              controller: inputData,
+            ),
+            TextButton(
+                onPressed: () {
+                  widget.addName(inputData.text);
+                  Navigator.pop(context);
+                },
+                child: Text('완료')),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('취소')),
+          ],
         ),
       ),
     );
